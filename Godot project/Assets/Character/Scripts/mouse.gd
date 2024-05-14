91ef5695001eccbe8143c1209a6ccec457f1264c
George Kreye <george.kreye@du.edu>
2024-05-13 20:50:32 -0600

    Camera/orientation changing
    Mouse now changes mouse character's view and turns it

--- Godot project/Assets/Character/Scripts/mouse.gd
+++ Godot project/Assets/Character/Scripts/mouse.gd
@@ -10,15 +10,24 @@ extends CharacterBody3D
 ## Magnitude of effect gravity should have
 @export var grav_modifier: float = 1.0
 
+@export_category("Camera")
+## Mouse sensitivity for camera
+@export var camera_sensitivity: float = 0.005
+## Whether to invert mouse movement required to look up or down (default is mouse up to look down)
+@export var invert_vertical : bool = false
+var camera_look_input : Vector2
+
+
+# variables to create on ready
+@onready var camera : Camera3D = $Camera3D
 @onready var game_gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * grav_modifier
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	camera.rotation = Vector3(0,0,0) # ensure camera does not flip
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+# Called every physics update
+func _physics_process(delta):
 	# apply gravity
 	if not is_on_floor():
 		velocity.y -= game_gravity * delta
@@ -34,6 +43,20 @@ func _process(delta):
 	
 	# perform movement
 	move_and_slide()
+	
+	# change y rotation based on camera
+	var camera_rotation : float
+	if invert_vertical: # check for inverted vertical camera movement controls
+		camera_rotation = camera_look_input.x * -1
+	else:
+		camera_rotation = camera_look_input.x
+	rotate_y(camera_rotation * camera_sensitivity)
+	
+	# change camera vertical orientation
+	camera.rotate_x(camera_look_input.y * camera_sensitivity)
+	
+	# reset look input
+	camera_look_input = Vector2.ZERO
 
 # Determines 2D/lateral movement for a frame.
 func determine_movement() -> Vector3:
@@ -42,3 +65,10 @@ func determine_movement() -> Vector3:
 	
 	# create movement vector & return
 	return (transform.basis * Vector3(movement_input.x,0,movement_input.y)).normalized() * movement_speed
+	
+# handles mouse input
+func _unhandled_input(event):
+	# if unhandled event is mouse being moved, set look input to the change
+	if event is InputEventMouseMotion:
+		camera_look_input = event.relative
+	
