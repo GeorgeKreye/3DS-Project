da9715b1196eed58f2a2e69cb1d147d225137f59
George Kreye <george.kreye@du.edu>
2024-05-13 20:32:02 -0600

    Jumping
    Also ensuring character collider will not get knocked out of place by locking rotation on X and Z axes

--- Godot project/Assets/Character/Scripts/mouse.gd
+++ Godot project/Assets/Character/Scripts/mouse.gd
@@ -1,9 +1,16 @@
 extends CharacterBody3D
 
 @export_category("Lateral movement")
+## Movement speed on the ground
 @export var movement_speed : float = 5.0
 
-@onready var game_gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export_category("Vertical movement")
+## Vertical velocity to apply on jump
+@export var jump_force: float = 5.0
+## Magnitude of effect gravity should have
+@export var grav_modifier: float = 1.0
+
+@onready var game_gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * grav_modifier
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,18 +22,21 @@ func _process(delta):
 	# apply gravity
 	if not is_on_floor():
 		velocity.y -= game_gravity * delta
+		
+	# determine whether to jump this frame
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
 	
 	# determine 2D (lateral) movement
-	var movement = determine_movement(delta)
+	var movement = determine_movement()
 	velocity.x = movement.x
 	velocity.z = movement.z
 	
 	# perform movement
 	move_and_slide()
 
-
 # Determines 2D/lateral movement for a frame.
-func determine_movement(delta) -> Vector3:
+func determine_movement() -> Vector3:
 	# determine movement from inputs
 	var movement_input = Input.get_vector("move_left","move_right","move_forward","move_backward")
 	
