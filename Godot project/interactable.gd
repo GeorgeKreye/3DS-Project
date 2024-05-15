3622c6e79f642fa69d430401d9d9d6f699f6e6a3
George Kreye <george.kreye@du.edu>
2024-05-15 11:34:32 -0600

    Interaction input
    Also some better doc for the interactable script

--- Godot project/interactable.gd
+++ Godot project/interactable.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
 @export var interaction_range: float = 0.3
+
+# signals
 signal do_interaction
 signal can_interact
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	# do interaction if in range and button pressed
+	# send interaction signal if in range and button pressed
 	if in_interaction_range() and Input.is_action_just_pressed("interact"):
 		do_interaction.emit()
 
@@ -17,5 +19,5 @@ func in_interaction_range() -> bool:
 	# check if close enough for interaction and return
 	var within_interaction_range = abs(player.position - position) <= interaction_range
 	if within_interaction_range:
-		can_interact.emit()
+		can_interact.emit() # send signal that interaction is possible
 	return within_interaction_range
