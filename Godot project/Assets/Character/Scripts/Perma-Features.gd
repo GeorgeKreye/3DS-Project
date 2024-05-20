64886579650f67e09d51b9b1231387c322499e2f
Orangeman102 <ftbryden@gmail.com>
2024-05-20 11:39:46 -0600

    Scene changes
    No fade in/out; two bugs and idk what they are but it works

--- Godot project/Assets/Character/Scripts/Perma-Features.gd
+++ Godot project/Assets/Character/Scripts/Perma-Features.gd
@@ -3,9 +3,12 @@ extends Node3D
 #@onready var fade = $Control/AnimationPlayer
 
 
-signal fadeEnd
+var scene2 = preload("res://Assets/Scenes/destroyed_scene.tscn")
 
-var count = 0
+#signal fadeEnd
+
+#var count = 0
+
 
 # Called when the node enters the scene tree for the first time.
 
@@ -13,22 +16,29 @@ var count = 0
 	
 
 #func fader():
-	#fader().play("Fade out")
+	#fade.play("FadeOut")
 	
 
 #func _on_animation_player_animation_finished(anim_name):
 	#if anim_name == "Fade out":
-	#	emit_signal("fadeEnd")
-	#	fader().play("Fade in")
+		#emit_signal("fadeEnd")
+		#fader().play("Fade in")
 
 
 #func _on_area_3d_body_entered(body):
 
+#func _change
 
 func _on_body_entered(body):
 	if body.is_in_group("Mouse"):
 		#fader()
 		#await fadeEnd
-		get_tree().change_scene_to_file("res://Assets/Scenes/destroyed_scene.tscn")
-		count+=1
-		print(count)
+		#get_tree().change_scene_to_file("res://Assets/Scenes/destroyed_scene.tscn")
+		get_tree().change_scene_to_packed(scene2)
+	
+
+
+#func _on_animation_player_animation_finished(anim_name):
+	#if anim_name == "FadeOut":
+		#emit_signal("fadeEnd")
+		#fade.play("FadeIn")
