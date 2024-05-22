82f3fb659fa743ab45ffc857c5055e785d84c34a
FossyChan <68196544+FossyChan@users.noreply.github.com>
2024-05-22 11:48:45 -0600

    added sounds to actions

--- Godot project/Assets/Character/Scripts/Perma-Features.gd
+++ Godot project/Assets/Character/Scripts/Perma-Features.gd
@@ -4,11 +4,12 @@ extends Node3D
 
 
 var scene2 = preload("res://Assets/Scenes/destroyed_scene.tscn")
+@onready var happyMouse = $Area3D/HappyMouse
+
 
 #signal fadeEnd
 
 #var count = 0
-
 
 # Called when the node enters the scene tree for the first time.
 
@@ -31,6 +32,11 @@ var scene2 = preload("res://Assets/Scenes/destroyed_scene.tscn")
 
 func _on_body_entered(body):
 	if body.is_in_group("Mouse"):
+		
+		happyMouse.play()
+		await(get_tree().create_timer(2).timeout)
+		
+		#set_modulate(lerp(get_modulate(), Color(0,0,0,1), 0.2))
 		#fader()
 		#await fadeEnd
 		#get_tree().change_scene_to_file("res://Assets/Scenes/destroyed_scene.tscn")
