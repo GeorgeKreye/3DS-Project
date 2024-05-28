edf69fdd8b1aa1a3ecc8c583ac050f96399638d7
Orangeman102 <ftbryden@gmail.com>
2024-05-28 17:36:19 -0600

    Added Textured House + Text/Transitions to all scenes

--- Godot project/Assets/Scripts/interactable.gd
+++ Godot project/Assets/Scripts/interactable.gd
@@ -8,17 +8,17 @@ signal do_interaction
 signal can_interact
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+#func _process(_delta):
 	# send interaction signal if in range and button pressed
-	if in_interaction_range() and Input.is_action_just_pressed("interact"):
-		do_interaction.emit()
+	#if in_interaction_range() and Input.is_action_just_pressed("interact"):
+		#do_interaction.emit()
 
-func in_interaction_range() -> bool:
+#func in_interaction_range() -> bool:
 	# get player object
-	var player : CharacterBody3D = get_node("Mouse")
+	#var player : CharacterBody3D = get_node("Mouse")
 	
 	# check if close enough for interaction and return
-	var within_interaction_range = abs(player.position - position) <= interaction_range
-	if within_interaction_range:
-		can_interact.emit() # send signal that interaction is possible
-	return within_interaction_range
+	#var within_interaction_range = abs(player.position - position) <= interaction_range
+	#if within_interaction_range:
+		#can_interact.emit() # send signal that interaction is possible
+	#return within_interaction_range
---
+++ Godot project/Scene3to4.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var scene4 = preload("res://Assets/Scenes/house_scene.tscn")
+
+var count = 0
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
+
+
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Mouse"):
+		#fader()
+		#await fadeEnd
+		#get_tree().change_scene_to_file("res://Assets/Scenes/destroyed_scene.tscn")
+		get_tree().change_scene_to_packed(scene4)
+		count += 1
+		print(count)
