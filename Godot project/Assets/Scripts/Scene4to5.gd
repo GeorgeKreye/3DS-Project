eec3811d5565a4523d37adbdc10a7389974c53d3
Orangeman102 <ftbryden@gmail.com>
2024-05-29 09:28:20 -0600

    Stair Platforming Outside House

--- Godot project/Assets/Scripts/Scene4to5.gd
+++ Godot project/Assets/Scripts/Scene4to5.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var scene5 = preload("res://Assets/Scenes/field_graybox_Time-Pass.tscn")
+var scene5 = preload("res://Assets/Scenes/field_graybox_TimePass.tscn")
 
 var count = 0
 
