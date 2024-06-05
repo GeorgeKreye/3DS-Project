d6f851365de2ab358050450d4c55e3fbc8ec4227
FossyChan <68196544+FossyChan@users.noreply.github.com>
2024-06-05 10:28:59 -0600

    added text_area_cracker

---
+++ Godot project/Assets/Scenes/text_area.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@onready var label = $"../Control/Label"
+
+#var timer = Time.new()
+
+func _on_body_entered(body):
+	if body.is_in_group("Mouse"):
+		label.visible = true
+		await get_tree().create_timer(5.0).timeout
+		label.visible = false
+		queue_free()
+
